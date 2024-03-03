d1b30acb36d9274c8d4abee01a0b86179446b6d0
jh177 <jameshong177@gmail.com>
2024-03-02 21:51:08 -0800

    Edit canvas size and add scroll button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import { Application } from "@splinetool/runtime";
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas3d");
   const formContainer = document.querySelector(".form-container");
+  const scrollDownBtn = document.getElementById("scroll-down-btn");
+  const formDiv = document.querySelector(".form-container");
+
+  // Initial position of the button at the bottom of the viewport
+  scrollDownBtn.addEventListener("click", function () {
+    // Calculate the position of the form div relative to the viewport
+    const formDivPosition =
+      formDiv.getBoundingClientRect().top + window.scrollY;
+
+    // Scroll the viewport to the top of the form div
+    window.scrollTo({ top: formDivPosition, behavior: "smooth" });
+  });
 
   const app = new Application(canvas);
   app
@@ -11,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // Defer the display of the form
       setTimeout(() => {
         formContainer.style.display = "flex";
+        scrollDownBtn.style.visibility = "visible";
       }, 2000); // Adjust the delay as needed
     })
     .catch((error) => {
